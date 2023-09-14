cd9d150ad9b04bf68a18ef936a3fe3f3f6697e8a
Codezz <112660193+Codezz-ops@users.noreply.github.com>
2023-09-14 10:40:11 -0400

    Add files via upload

---
+++ build/build.ps1
@@ -0,0 +1,55 @@
+param (
+    [string]$userInput
+)
+
+$projectName = "Pshark"
+$windowspythonScript = "windows\main.py"
+$linuxpythonScript = "linux\main.py"
+$outputFolder = ".\dist"
+$buildCommand = "pyinstaller"
+
+function Remove-BuildFolder {
+    if (Test-Path -PathType Container $outputFolder) {
+        Write-Host "Removing build folder: $outputFolder"
+        Remove-Item -Path $outputFolder -Force -Recurse
+    }
+}
+
+if (-not (Test-Path -PathType Container $outputFolder)) {
+    New-Item -ItemType Directory -Force -Path $outputFolder
+}
+
+$requiredPackages = "pyinstaller", "scapy", "psutil"
+$installedPackages = Get-Content "requirements.txt" | ForEach-Object { $_.Trim() }
+$missingPackages = $requiredPackages | Where-Object { $_ -notin $installedPackages }
+
+if ($missingPackages.Count -gt 0) {
+    Write-Host "Installing missing packages: $($missingPackages -join ', ')"
+    $missingPackages | ForEach-Object {
+        Add-Content -Path "requirements.txt" -Value $_
+        pip install $_
+    }
+}
+
+if ($userInput -eq "windows" -or $userInput -eq "linux") {
+    Write-Host "Building $projectName for $userInput..."
+
+    $pythonScript = if ($userInput -eq "windows") { $windowspythonScript } else { $linuxpythonScript }
+    $buildCmd = "$buildCommand $pythonScript --onefile --name $projectName --distpath $outputFolder --clean"
+
+    Invoke-Expression -Command $buildCmd
+}
+elseif ($userInput -eq "clean") {
+    $itemsToRemove = @(".\dist", ".\build\Pshark", "Pshark.spec")
+    
+    foreach ($item in $itemsToRemove) {
+        Remove-Item $item -Force -Recurse
+    }
+}
+else {
+    Write-Host "Invalid target platform specified. Use 'windows' or 'linux'."
+    Exit 1
+}
+
+Write-Host "Build completed for $userInput. Executable is located in: $outputFolder"
+Exit 0
---
+++ linux/main.py
@@ -0,0 +1,64 @@
+import sys
+import psutil
+import socket
+from scapy.all import sniff
+
+def get_version_number():
+    with open('.version', 'r') as file:
+        contents = file.read()
+
+        for line in contents.split("\n"):
+            key, value = line.split("=")
+
+            if key.strip() == "VERSION":
+                return value.strip()
+
+    return None
+
+def print_network_interfaces():
+    interfaces = psutil.net_if_addrs()
+    for interface, addresses in interfaces.items():
+        print(f"Interface: {interface}")
+        for address in addresses:
+            print(f"  - Address: {address.address}")
+            print(f"    Family: {address.family}")
+            print(f"    Netmask: {address.netmask}")
+            print(f"    Broadcast: {address.broadcast}")
+
+def get_valid_network_interface():
+    interfaces = psutil.net_if_addrs()
+    valid_interfaces = [name for name, addrs in interfaces.items() if not any(socket.AF_INET in (addr.family, addr.address.startswith('127.')) for addr in addrs)]
+
+    if valid_interfaces:
+        return valid_interfaces[0]
+    else:
+        raise ValueError("No valid network interface found.")
+
+COMMANDS = {
+    "start": lambda: sniff(iface=get_valid_network_interface(), prn=lambda x: x.summary()),
+    "info": print_network_interfaces,
+    "version": lambda: print(f"Current version: {get_version_number()}"),
+    "exit": exit,
+    "help": lambda: print_help(),
+}
+
+def print_help():
+    print("help:     Prints out this menu")
+    print("start:    Starts the packet sniffer")
+    print("version:  Prints out current version")
+    print("info:     Prints out all your network interfaces info")
+    print("exit:     Quit app")
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == "/?":
+        print_help()
+
+    while True:
+        userInput = input("> ")
+        if userInput in COMMANDS:
+            COMMANDS[userInput]()
+        else:
+            print("Invalid command. Use 'start' to begin sniffing or 'info' to get network information.")
+
+if __name__ == "__main__":
+    main()
